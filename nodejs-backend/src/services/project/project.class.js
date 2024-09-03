b5126c36c3340e90b04066cc8c36722fc29d4519
CodeBridgeGenerator <kana@codebridge.my>
2024-09-03 04:13:16 +0000

    CodeBridge commit

---
+++ nodejs-backend/src/services/project/project.class.js
@@ -0,0 +1,5 @@
+const { Service } = require('feathers-mongoose');
+
+exports.Project = class Project extends Service {
+  
+};
---
+++ nodejs-backend/src/services/project/project.service.js
@@ -0,0 +1,20 @@
+const { Project } = require('./project.class');
+const createModel = require('../../models/project.model');
+const hooks = require('./project.hooks');
+
+module.exports = function (app) {
+  const options = {
+    Model: createModel(app),
+    paginate: app.get('paginate'),
+    whitelist: ["$populate"],
+    multi: ["create"],
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/project', new Project(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('project');
+
+  service.hooks(hooks);
+};
